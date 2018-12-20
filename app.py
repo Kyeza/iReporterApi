d5ba322a698976aa8c19e064dcf4c053c9c0b4ed
kyeza <kyezaarnold63@gmail.com>
2018-12-20 16:07:57 +0300

    [#162758220] Finished get a specific redflag endpoint

--- app.py
+++ app.py
@@ -28,19 +28,32 @@ def get_red_flags():
     return jsonify(result), 200
 
 
+@app.errorhandler(InvalidApiUsage)
 @app.route('/ireporter.com/api/v1/red-flags', methods=['POST'])
 def create_red_flags():
     if not request.json or not 'type' in request.json:
-        try:
-            raise InvalidApiUsage('Not a valid red-flag')
-        except InvalidApiUsage as e:
-            print(e.to_dict())
-            return jsonify(e.to_dict())
+        raise InvalidApiUsage('Not a valid red-flag')
 
     obj = Incident(**request.json)
     db.append(obj.to_dict)
     result = {'status': 201, 'data': obj.to_dict}
     return jsonify(result), 201
+
+@app.errorhandler(InvalidApiUsage)
+@app.route('/ireporter.com/api/v1/red-flags/<int:incident_id>', methods=['GET'])
+def get_red_flag(incident_id):
+    red_flag = [red_flag for red_flag in db if red_flag['id'] == incident_id]
+    if len(red_flag) == 0:
+        raise InvalidApiUsage(f'red-flag with id={incident_id} not fouund', status_code=404)
+    result = {'status': 200, 'data': red_flag}
+    return jsonify(result), 200
+
+
+@app.errorhandler(InvalidApiUsage)
+def handle_invalid_usage(error):
+    result = jsonify(error.to_dict())
+    result.status_code = error.status_code
+    return result
 
 
 if __name__ == '__main__':
